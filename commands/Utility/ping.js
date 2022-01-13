9c54e50a72e2d696edaddec4a0586dc5d7d0a040
Callum <callumnewsome100@gmail.com>
2022-01-13 00:17:43 +0000

    Ping command

---
+++ commands/Utility/ping.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "ping",
+    aliases: ["pong", "latency"],
+    category: "Utility",
+    description: "Check the bot's ping!",
+    run: async (client, message, args) => {
+        const msg = await message.channel.send(`🏓 Pinging...`);
+
+        const pingEmbed = new client.discord.MessageEmbed()
+            .setTitle(':signal_strength: Bot Ping')
+            .addField("Time", `${Math.floor(msg.createdAt - message.createdAt)}ms`, true)
+            .addField("API Ping", `${client.ws.ping}ms`, true)
+            .setColor(client.config.embedColor)
+            .setFooter({
+                text: client.config.embedfooterText,
+                iconURL: client.user.avatarURL()
+            });
+
+        await message.reply({ embeds: [pingEmbed], allowedMentions: { repliedUser: false } });
+
+        msg.delete();
+    },
+}
